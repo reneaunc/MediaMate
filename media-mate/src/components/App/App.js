f55d6bb4c521627b319321e79d561e4d9e9fabb8
Noah Reneau <reneaunc@gmail.com>
2023-02-03 18:15:32 -0800

    Added Media Info Card

--- media-mate/src/components/App/App.js
+++ media-mate/src/components/App/App.js
@@ -4,6 +4,7 @@ import Landing from '../Landing/Landing';
 import LandingNavBar from '../LandingNavBar/LandingNavBar';
 import LandingFooter from '../LandingFooter/LandingFooter';
 import Login from '../Login/Login';
+import MediaInfo from "../Pages/MediaInfo/MediaInfo";
 import { Route, Routes } from 'react-router-dom';
 
 
@@ -17,6 +18,7 @@ class App extends React.Component {
           <Route path="/" element={<Landing />}></Route>
           <Route path='/login' element={<Login />}></Route>
         </Routes>
+        <MediaInfo />
         <LandingFooter />
       </div>
     );
