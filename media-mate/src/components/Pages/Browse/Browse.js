299c338fa8cb167cd31aeb14170be0605c8db483
Noah Reneau <reneaunc@gmail.com>
2023-03-02 18:31:49 -0800

    Added SearchBar in Browse, needs styling

--- media-mate/src/components/Pages/Browse/Browse.js
+++ media-mate/src/components/Pages/Browse/Browse.js
@@ -1,11 +1,13 @@
 import MediaCard from "../MediaCard/MediaCard";
 import styles from './Browse.module.css'
+import SearchBar from "../../Searchbar/Searchbar";
 
 function Browse(props) {
     console.log(props);
     return (
         <>
         <h1>Browse</h1>
+        <SearchBar />
         <div className={styles.browseArea}>
             {props.cards.map((cur)=> {
                 return <MediaCard curMedia={cur} />
