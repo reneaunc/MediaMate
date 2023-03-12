ba165e02e807e19d09f50e92d7c7ccad9c9f5e3e
Noah Reneau <reneaunc@gmail.com>
2023-03-12 16:59:21 -0700

    User can no longer go to library unless logged in, also cleaned up comments inf Feed-Browse

--- media-mate/src/components/Pages/Feed/Feed.js
+++ media-mate/src/components/Pages/Feed/Feed.js
@@ -7,7 +7,7 @@ import allMedias from "../../../util/getAllMedias";
 import getUserWishlist from "../../../util/getUserWishlist";
 import allHistories from "../../../util/getHistories";
 
-function Feed(props) {
+function Feed() {
     //const [users, setUsers] = useState([]);
     //const [medias, setMedias] = useState([]);
     //const [media, setMedia] = useState([]);
@@ -21,99 +21,10 @@ function Feed(props) {
         //allMedias.getAllMedias().then((medias) => setMedias(medias));
         //fillFeedArr(users, medias).then((userMedia) => setUserMedia(userMedia));     
     }, [])
-    /*
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max)
-    }
-    function getMediaFromTitle(title, medias){
-        for(const media in medias){
-            if(medias[media].title === title){
-                return medias[media]
-            }
-        }
-    }
-    async function fillFeedArr(users, medias) {
-        const mediaArr = []
-        const usersUsed = []
-        for(let i = 0; i < 4; i++){
-            let userNum = getRandomInt(users.length)
-            //console.log(users)
-            if(usersUsed.includes(users[userNum].username)){
-                continue
-            }
-            //console.log(`userNum is ${userNum}`)
-            let arrType = getRandomInt(2)
-            //console.log(`arrtype is ${arrType}`)
-            if(arrType === 1){
-                if(users[userNum].consume.length === 0){
-                    continue;
-                }
-                let media = users[userNum].consume[users[userNum].consume.length - 1]
-                media = getMediaFromTitle(media, medias)
-                let postObj = {
-                    user: users[userNum].username,
-                    media: media,
-                    library: "consume"
-                }
-                mediaArr.push(postObj)
-                usersUsed.push(users[userNum].username)
-            } else {
-                if(users[userNum].wishlist.length === 0){
-                    continue;
-                }
-                let media = users[userNum].wishlist[users[userNum].wishlist.length - 1]
-                media = getMediaFromTitle(media, medias)
-                let postObj = {
-                    user: users[userNum].username,
-                    media: media,
-                    library: "wishlist"
-                }
-                mediaArr.push(postObj)
-                usersUsed.push(users[userNum].username)
-            }
-        }
-        return mediaArr
-    }
-    useEffect(() => {
-        allUsers.getAllUsers().then((users) => setUsers(users));
-        allMedias.getAllMedias().then((medias) => setMedias(medias));
-        fillFeedArr(users, medias).then((userMedia) => setUserMedia(userMedia));     
-    }, [])*/
-    
-    
-    
-    // const infoArr = []
-    // const userList = mediaTitles.list
-    // for(const userMedia in userList){
-    //     for(const media in medias){
-    //         if(medias[media].title === userList[userMedia]){
-    //             infoArr.push(medias[media])
-    //         }
-    //     }
-    // }
-    //console.log(`User Media: ${userMedia}`)
     
     return (
         <>
         <h1>Feed</h1>
-        {/* <div>
-            {activities.slice(-4).map((history) => console.log(history))}
-        </div> */}
-        {/* <div>
-            <p>{`${media.id}`} & {`${media.title}`}</p>
-        </div>
-        <div>
-            {users.map((user) => <p> {user.id} & {user.username} & {user.email}</p>)}
-        </div> */}
-        {/* <div>
-            {medias.map((media) => <p> {media.id} & {media.title} & {media.releaseYear}</p>)}
-        </div> */}
-        {/* <div>
-            {infoArr.map((media) => <p> {JSON.stringify(media)} </p>)}
-        </div> */}
-        <div>
-            {/* {userMedia.list[userMedia.list.length - 1]} */}
-        </div>
         <div className={styles.feedArea}>
             {activities.slice(-4).map((history)=> {
                 return <Post curPost={history} />
